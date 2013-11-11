33cf79fd383999a9f88f11a6aa57a9bebac131d8
Kevin Filella <kfilella@espol.edu.ec>
2013-11-11 13:09:52 -0500

    commit de actualizacion

--- workspace/Buscaminas/src/com/example/buscaminas/Juego.java
+++ workspace/Buscaminas/src/com/example/buscaminas/Juego.java
@@ -1,9 +1,13 @@
 package com.example.buscaminas;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.example.buscaminas.R;
 
 import android.R.layout;
 import android.app.Activity;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.view.View;
@@ -18,32 +22,37 @@ public class Juego extends Activity {
 	private TextView tiempo;
 	private TextView puntaje;
 	TableLayout tablero;
-	private celda bloque[][];
-	private celda n1;
+	private Celda matriz[][] = new Celda[7][7];
+	private Celda n1;
 	
-	View[] buttonsAndEditboxes = new View[7];
+	View[][] matrizCeldas = new View[7][7];
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.juego);
         tiempo=(TextView)this.findViewById(R.id.tiempo);
+        tiempo.setText("00:00");
+        tiempo.setTextColor(Color.GREEN);
+
         puntaje=(TextView)this.findViewById(R.id.puntaje);
+        puntaje.setText("PUNTAJE");
+        puntaje.setTextColor(Color.GREEN);
         tablero=(TableLayout)this.findViewById(R.id.tabla);
+        
         
         //Typeface lcdFont = Typeface.createFromAsset(getAssets(),  "fonts/lcd2mono.ttf");
         //tiempo.setTypeface(lcdFont);
         //puntaje.setTypeface(lcdFont);
         
         for (int i = 0; i < 7; i++) {
-        	   
-        	        buttonsAndEditboxes[i]= new Button(this);
-        	      
+        	TableRow fila = new TableRow(this);
+        	for (int j=0; j<7; j++){
+        		matriz[i][j] = new Celda(this,i,j,0);
+        		matrizCeldas[i][j]= matriz[i][j];
+        		fila.addView(matrizCeldas[i][j]);
         	}
-        
-        for (int i = 0; i < 7; i++) {
-        	   tablero.addView(buttonsAndEditboxes[i]);
-        	}
-        		
-        
+        	tablero.addView(fila);
+    	}
+
     }
 }
--- workspace/Buscaminas/src/com/example/buscaminas/celda.java
+++ workspace/Buscaminas/src/com/example/buscaminas/celda.java
@@ -1,16 +1,43 @@
 package com.example.buscaminas;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
 import android.widget.Button;
 
-public class celda extends Button {
-
-	public celda(Context context) {
+public class Celda extends Button {
+	int pos_x;
+	int pos_y;
+	int valor;
+	public Celda(Context context, int posx, int posy, int val) {
 		super(context);
-		// TODO Auto-generated constructor stub
+		this.pos_x = posx;
+		this.pos_y = posy;
+		this.valor = val;
 	}
-
-	
-
+	public void descubrirAdyacentes(Celda celda, Celda[][] matriz, int ancho, int alto){
+		if(celda.isPressed()){
+			if(celda.valor!=-1){
+				if(celda.valor==0){
+					celda.setPressed(true);
+					if (matriz[celda.pos_x-1][celda.pos_y-1].valor==0)
+						descubrirAdyacentes(matriz[celda.pos_x-1][celda.pos_y-1], matriz, ancho, alto);
+					if (matriz[celda.pos_x-1][celda.pos_y].valor==0)
+						descubrirAdyacentes(matriz[celda.pos_x-1][celda.pos_y], matriz, ancho, alto);
+					if (matriz[celda.pos_x][celda.pos_y-1].valor==0)
+						descubrirAdyacentes(matriz[celda.pos_x][celda.pos_y-1], matriz, ancho, alto);
+					if (matriz[celda.pos_x+1][celda.pos_y].valor==0)
+						descubrirAdyacentes(matriz[celda.pos_x+1][celda.pos_y], matriz, ancho, alto);
+					if (matriz[celda.pos_x][celda.pos_y+1].valor==0)
+						descubrirAdyacentes(matriz[celda.pos_x][celda.pos_y+1], matriz, ancho, alto);
+					if (matriz[celda.pos_x+1][celda.pos_y+1].valor==0)
+						descubrirAdyacentes(matriz[celda.pos_x+1][celda.pos_y+1], matriz, ancho, alto);
+					if (matriz[celda.pos_x+1][celda.pos_y-1].valor==0)
+						descubrirAdyacentes(matriz[celda.pos_x+1][celda.pos_y-1], matriz, ancho, alto);
+					if (matriz[celda.pos_x-1][celda.pos_y+1].valor==0)
+						descubrirAdyacentes(matriz[celda.pos_x-1][celda.pos_y+1], matriz, ancho, alto);
+				}
+			}	
+		}
+	}
 }
